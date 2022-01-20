2680309ea6996d4e1101b0bf9e572df8544311bc
Tina Shumilova <shutiva@gmail.com>
2022-01-20 21:22:00 +0200

    final version of first seminar

--- seminar_20_01_22/seminar_1.py
+++ seminar_20_01_22/seminar_1.py
@@ -1,64 +1,68 @@
 #0.	Вывести квадрат числа
-#number = int(input('Add nummer >> '))
-#print(f'Квадрат числа {number} равен {number**2}')
+number = int(input('Add nummer >> '))
+print(f'Квадрат числа {number} равен {number**2}')
 
 # 2.	Даны два числа. Показать большее и меньшее число
-# a = 9
-# b = 10
-# if a>b:
-#     print(f'max = {a}, min = {b}')
-# else:
-#     print(f'max = {b}, 'min = {a}')
+a = 9
+b = 10
+if a>b:
+    print(f'max = {a}, min = {b}')
+else:
+    print(f'max = {b}, min = {a}')
 
 # 4.	Найти максимальное из трех чисел
-# c = 15
-# if a > b and a > c:
-#     print(f'max = {a}')
-# elif a < b and b > c:
-#     print(f'max = {b}')
-# else:
-#     print(f'max = {c}')
+c = 15
+if a > b and a > c:
+    print(f'max = {a}')
+elif a < b and b > c:
+    print(f'max = {b}')
+else:
+    print(f'max = {c}')
 
 # 6.	Выяснить является ли число чётным
 
-# a = 5
-# if a%2==0:
-#     print('число чётное ')
-# else:
-#     print('число нечётное ')
+a = 5
+if a%2==0:
+    print('число чётное ')
+else:
+    print('число нечётное ')
 
 # 8.	Показать четные числа от 1 до N
 
-# N = int(input('Введите конечное число - '))
-# r = range(1, N +1)
-# for i in r:
-#     if i%2==0:
-#         print(i)
+N = int(input('Введите конечное число - '))
+r = range(1, N +1)
+for i in r:
+    if i%2==0:
+        print(i)
 
 #10.	Показать вторую цифру трёхзначного числа
 
-# d = -268
-# number = d//10
-# print(number%10)
+d = 268
+number = d//10
+print(number%10)
 
 #12.	Удалить вторую цифру трёхзначного числа
 
-# number3 = d % 10
-# number1 = d // 100
-# print(number1 * 10 + number3)
+number3 = d % 10
+number1 = d // 100
+print(number1 * 10 + number3)
 
 #14.	Найти третью цифру числа или сообщить, что её нет
 
 e = list(input('Введите число - '))
-print(len(e))
-print(e[2])
+lenght = len(e)
+if lenght < 3:
+    print('третьего число нет')
+else:
+    print(e[2])
 
-#16. Дано число. Проверить кратно ли оно 7 и 23
 
-# g = 161
+#15. Дано число. Проверить кратно ли оно 7 и 23
 
-# if g%7==0 and g%23 ==0:
-#     print(f'число {g} кратно 7 и 23')
-# else:
-#     print(f'число {g} некратно 7 и 23')
+g = 161
+
+if g%7==0 and g%23 ==0:
+    print(f'число {g} кратно 7 и 23')
+else:
+    print(f'число {g} некратно 7 и 23')
 
