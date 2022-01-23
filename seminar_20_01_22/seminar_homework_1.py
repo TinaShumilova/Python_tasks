0f6f1248268022ed98d37b5ebf1193cf41feb941
Tina Shumilova <shutiva@gmail.com>
2022-01-23 16:33:25 +0200

    Tasks 5

--- seminar_20_01_22/seminar_homework_1.py
+++ seminar_20_01_22/seminar_homework_1.py
@@ -7,15 +7,24 @@
 # print(Task_1(a, b))
 
  #3. По заданному номеру дня недели вывести его название
-def day_title(x):
-    titles = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье']
-    if 0 < x < 8:
-        x -= 1
-        return titles[x]
-    else: return 'Неверно выбранный день недели.'
+# def day_title(x):
+#     titles = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье']
+#     if 0 < x < 8:
+#         x -= 1
+#         return titles[x]
+#     else: return 'Неверно выбранный день недели.'
 
-c = int(input('Введите номер дня недели - '))
-print(day_title(c))
+# c = int(input('Введите номер дня недели - '))
+# print(day_title(c))
+
+#5. Написать программу вычисления значения функции y = f(a)
+import math
+
+def find_cosinus(x):
+    return f'cos{x} = {math.cos(x)}'
+
+d = int(input('число = '))
+print(find_cosinus(d))
 
 
         
