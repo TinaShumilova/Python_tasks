97d3176cd031b74c224baffb42a85a5c4b10b638
Tina Shumilova <shutiva@gmail.com>
2022-01-23 17:17:33 +0200

    Tasks 9

--- seminar_20_01_22/seminar_homework_1.py
+++ seminar_20_01_22/seminar_homework_1.py
@@ -27,18 +27,27 @@ import math
 # print(find_cosinus(d))
 
 #7. Показать числа от -N до N
-def show_number(x, y):
-    if x < 0 and y >= 0:
-        for i in range(x, y+1):
-            print(i)
-    else: print('неверно заданные параметры')
+# def show_number(x, y):
+#     if x < 0 and y >= 0:
+#         for i in range(x, y+1):
+#             print(i)
+#     else: print('неверно заданные параметры')
 
-e = int(input('-N = '))
-g = int(input('N = '))
-show_number(e, g)
+# e = int(input('-N = '))
+# g = int(input('N = '))
+# show_number(e, g)
 
 #9. Показать последнюю цифру трёхзначного числа
+def find_last_number_of_hundreds(x):
+    if 1000 > x >= 100:
+        return x%10
+    elif -1000 < x <= -100:
+        x *= -1
+        return x%10
+    else: return 'неверно заданное число, введите трёхзначное число'
 
+h = int(input('Введите трёхзначное число = '))
+print(find_last_number_of_hundreds(h))
 
 
 
