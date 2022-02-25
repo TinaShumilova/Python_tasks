a198cc9654e0175ff2d555822ca01e175c4978bc
Tina Shumilova <shutiva@gmail.com>
2022-02-25 16:01:44 +0200

    Task45_final

--- semina_31_01_22/seminar_4_homework/Task_45.py
+++ semina_31_01_22/seminar_4_homework/Task_45.py
@@ -3,10 +3,8 @@ num_10 = int(input('Введите чирсло - '))
 def find_binary(num_10):
     num_2 = ''
     temp_num = num_10
-    temp = 0
     while temp_num != 0:
-        temp = temp_num % 2
-        num_2 += str(temp)
+        num_2 += str(temp_num % 2)
         temp_num //= 2
     num_2 = num_2[::-1]
     return num_2
